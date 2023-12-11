acb84763e972328e07f0fc47c00fe22631be093d
Faisal Banjar Windows Laptop <FaisalBj1@outlook.com>
2023-12-11 05:04:05 +0300

    project - creating gallery

--- main.js
+++ main.js
@@ -62,25 +62,26 @@ class MyHeader extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <style>
-                body {
-                    
-                }
                 my-header {
+                    z-index: 9999999;
                     width: 100%;
+                    padding: 9px 15px;
                     margin-bottom: auto;
                     border-bottom: solid 0.25px var(--primary-color);
+                    background: transparent;
                     backdrop-filter: blur(15px);
                 }
                 my-header * {
                     color: var(--primary-color);
+                    box-sizing: border-box;
                     text-decoration: none;
                 }
                 nav {
                     display: grid;
                     grid-template-areas: 'logo nav_menu icons';
-                    padding: 0 1.5rem;
                     background: transparent;
                     margin: 0;
+                    padding: 0;
                     justify-content: stretch;
                 }
                 nav .logo {
@@ -109,7 +110,7 @@ class MyHeader extends HTMLElement {
                     display: flex;
                     flex-direction: row;
                     gap: 21px;
-                    transform: translate(-4rem);
+                    transform: translate(-3rem);
                     place-content: center;
                 }
                 nav .menu * {
@@ -180,18 +181,17 @@ class MyHeader extends HTMLElement {
                         grid-template-areas: 
                         'logo icons'
                         'nav_menu nav_menu';
-                        padding: 0.5rem 1.5rem;
                     }
                     nav .menu {
                         display: none;
                     }
                     nav .menu ul{
                         flex-direction: column;
-                        transform: translate(-1.5rem);
+                        transform: translate(0);
                     }
                     nav #toggle-02:checked ~ .menu {
                         display: block;
-                        animation: menu_slide 0.75s ease-out;
+                        animation: fade_in .75s ease-out;
                     }  
                 }
                 /* [phone] */
@@ -203,31 +203,19 @@ class MyHeader extends HTMLElement {
                         grid-template-areas: 
                         'logo icons'
                         'nav_menu nav_menu';
-                        padding: 0.5rem 1.5rem;
                     }
                     nav .menu {
                         display: none;
                     }
                     nav .menu ul{
                         flex-direction: column;
-                        transform: translate(-1.5rem);
+                        transform: translate(0);
                     }
                     nav #toggle-02:checked ~ .menu {
                         display: block;
-                        animation: menu_slide 0.75s ease-out;
+                        animation: fade_in .75s ease-out;
                     }  
                 }
-                       
-                @keyframes menu_slide {
-                    from {
-                        opacity: 0;
-                        transform: translateY(-17px);
-                    } to {
-                        opacity: 1;
-                        transform: translateY(0);
-                    }
-                }
-
             </style>
 
             <nav> 
@@ -279,25 +267,24 @@ class MyFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <style>
-                body {
-                    
+                my-footer{
+                    z-index: 9999999;
+                    box-sizing: border-box; 
+                    width: 100%; 
+                    margin-top: auto;
+                    background: transparent;
+                    backdrop-filter: blur(15px);
                 }
-
-                footer, my-footer{
+                footer {
                     border-top: solid 1px var(--primary-color);
                     backdrop-filter: blur(15px);
-                    box-sizing: border-box;
                     display: flex;
                     flex-direction: column;
-                    justify-content:space-evenly;
+                    justify-content: space-between;
                     text-align: center;
                     align-items: center;
-                    
-                    gap: 1em;
-                    padding: 0;
-                    margin-top: auto; 
-                    width: 100%;
-                    // background: linear-gradient(0, var(--primary-color), rgba(0, 0, 0, 0.0));
+                    gap: 1.5rem;
+                    padding: 25px 25px 11px 25px;
                 }
                     footer a {
                         color: var(--accent-color);
@@ -307,47 +294,32 @@ class MyFooter extends HTMLElement {
                         color: var(--accent-color);
                         font-size: 0.75rem;
                     }
-                    footer .footer-heading {
-                        color: transparent;
-                        font-weight: 100;
-                        font-size: 2.5rem;
-                        -webkit-text-stroke: 0.025rem var(--text-color);
-                        text-transform: uppercase;
-                        letter-spacing: 3px;
-                    }
-                    .social_media_container {
+                    footer .social_media_container {
                         width: 100%;
                         display: flex;
                         flex-wrap: wrap;
                         justify-content: center;
                         gap: 7px;
                     }
-                    .social_media_container a i {
+                    footer .social_media_container a i {
                         font-size: 1.75rem;
                         color: var(--text-color);
                     }
-                    .social_media_container a i:hover {
+                    footer .social_media_container a i:hover {
                         transform: scale(1.15);
                         transition: transform 0.25s ease-out;
                         color: var(--accent-color);
                     }
-                    .social_media_container a i:not(hover) {
+                    footer .social_media_container a i:not(hover) {
                         transform: scale(1);
                         transition: transform 0.25s ease-out;
                     }
-
-                    p a {
+                    footer .copyright p a {
                         color: var(--text-color);
-
                     }
-
-
             </style>    
             
             <footer>
-                <!-- <a href="#">LOGO</a> -->
-                <br/>
-            
                 <div class="social_media_container">
                     <a href="https://facebook.com/" target="_blank" aria-placeholder="hello"><i class="fa-brands fa-square-facebook"></i></a>
                     <a href="https://instagram.com/FaisalBj1" target="_blank"><i class="fa-brands fa-square-instagram"></i></a>
@@ -357,8 +329,8 @@ class MyFooter extends HTMLElement {
                     <a href="https://www.linkedin.com/in/faisal-banjar-943b791ba/" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
                     <a href="https://github.com/FaisalBj1" target="_blank"><i class="fa-brands fa-square-github"></i></a>
                 </div>
-        
-                <div>
+
+                <div class='copyright'>
                     <p>Copyright ©${new Date().getFullYear()} All rights reserved | Desined & Developed by <a href="http://FaisalBj1.com" target="_blank">FaisalBj1</a></p>
                 </div>
             </footer>

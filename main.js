26a13d21e2cea69a0416fefbd0d38527c86c89cf
Faisal Banjar Windows Laptop <FaisalBj1@outlook.com>
2023-12-18 00:29:55 +0300

    all good

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ window.addEventListener('load', () => {
 
     root.setAttribute('data-theme', 'darkness');
 
-    if (localStorage.getItem("theme") === null){
+    if ((localStorage.getItem("theme") != 'light') && (localStorage.getItem("theme") != 'dark') && (localStorage.getItem("theme") != 'darkness')){
         root.setAttribute('data-theme', 'darkness');
     } else {
         root.setAttribute('data-theme', localStorage.getItem("theme"));
